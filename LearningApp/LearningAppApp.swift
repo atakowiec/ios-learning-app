b0d0dab85a0120043858d1ccfe07f0bdc3a8e8aa
atakowiec <mat.cieszczyk@gmail.com>
2024-06-03 13:18:33 -0700

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Mateusz on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
